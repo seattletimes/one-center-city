88fbeb69872314f54b8309566b7a9ee6381b6ecc
Thomas Wilburn <news@thomaswilburn.net>
2018-01-03 14:16:42 -0800

    Updates 1

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,9 @@
 require("savage-image");
 
 var $ = require("./lib/qsa");
+var debounce = require("./lib/debounce");
 var Camera = require("savage-camera");
+var savage = require("savage-query");
 
 var container = $.one(".map-container");
 var poiList = $(".poi").reverse();
@@ -14,23 +16,65 @@ var current = null;
 
 container.addEventListener("load", function() {
   var camera = new Camera($.one("svg", container));
+  var image = savage(".map-container image");
+  var svg = $.one("svg", container);
 
-  window.addEventListener("scroll", function() {
+  var m = savage.dom;
+  var dropshadow = m("filter", { id: "drop" },
+    [-1, -1, 1, 1].map((y, i) =>
+      m("feDropShadow", {
+        dx: y,
+        dy: i % 2 ? -1 : 1,
+        stdDeviation: .5,
+        "flood-color": "#D8D9DA",
+        "flood-opacity": 1
+      })
+    )
+  );
 
+  var defs = $.one("defs", svg);
+  defs.appendChild(dropshadow);
+
+  var desat = m("filter", { id: "desat" }, [
+    m("feColorMatrix", {
+      type: "saturate",
+      values: "0.5"
+    })
+  ]);
+  defs.appendChild(desat);
+
+
+  window.addEventListener("scroll", debounce(function() {
+
+    var located = false;
     for (var i = 0; i < poiList.length; i++) {
       var poi = poiList[i];
       var id = poi.getAttribute("data-map");
       if (!id) continue;
+      var layer = $.one("#" + id, container);
+      if (!layer) return console.log(id);
       var bounds = poi.getBoundingClientRect();
-      if (bounds.top < window.innerHeight) {
+      if (!located && bounds.top < window.innerHeight) {
         if (id == current) return;
-        current = id;
-        console.log(id, $.one("#" + id));
-        camera.zoomTo($.one("#" + id));
-        return;
+        located = layer;
+        savage(layer).addClass("highlight");
+      } else {
+        savage(layer).removeClass("highlight");
       }
     }
 
-  });
+    if (!located) {
+      current = null;
+      camera.pan({ x: 1, y: 1, width: 770.1, height: 909.3 }, 400);
+      image.removeClass("zoomed");
+    } else if (located != current) {
+      current = located;
+      var layerBounds = located.getBBox();
+      var padding = layerBounds.width * layerBounds.height > 20000 ? 10 : 50;
+      camera.zoomTo(located, padding);
+      image.addClass("zoomed");
+    }
+
+  }, 300));
 
 });
